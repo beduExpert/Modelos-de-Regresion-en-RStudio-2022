6565c1f9c4e82cc7c6f4f3d86584a229167999cd
Erick <e.coronadoj@gmail.com>
2022-01-17 16:52:46 -0600

    Add files via upload

---
+++ Sesion-02/Ejemplo-01/Ejemplo_1.R
@@ -0,0 +1,58 @@
+# Ejemplo 1. Regresión Lineal Múltiple
+
+# Predecir el precio de cena (platillo). 
+# Datos de encuestas de clientes de 168 restaurantes Italianos
+# en el área deseada están disponibles.
+
+# Y: Price (Precio): el precio (en USD) de la cena
+# X1: Food: Valuación del cliente de la comida (sacado de 30)
+# X2: Décor: Valuación del cliente de la decoración (sacado de 30)
+# X3: Service: Valuación del cliente del servicio (sacado de 30)
+# X4: East: variable dummy: 1 (0) si el restaurante está al este (oeste) de la quinta avenida
+
+# Primero debemos establecer nuestro directorio de trabajo y el archivo
+# de datos (nyc.csv) que importaremos a R deberá de estar en este directorio
+setwd("c:/DATA/Programacion-R-Santander-2021-main/Sesion-05/Ejemplo-01/")
+nyc <- read.csv("nyc.csv", header = TRUE)
+
+# Observamos algunas filas y la dimensión del data frame
+
+tail(nyc, 2) 
+dim(nyc)
+attach(nyc)
+
+# Acontinuación mostramos una matriz de gráficos de dispersión de los
+# tres predictores continuos y la variable de respuesta. 
+
+pairs(~ Price + Food + Decor + Service, data = nyc, gap = 0.4, cex.labels = 1.5)
+
+# Observamos relaciones aproximadamente lineales
+
+# Llevamos a cabo el ajuste de un modelo
+# Y = beta0 + beta1*Food + beta2*Decor + beta3*Service + beta4*East + e
+
+m1 <- lm(Price ~ Food + Decor + Service + East)
+
+# Obtenemos un resumen
+
+summary(m1)
+
+# Ajustamos nuevamente un modelo pero ahora sin considerar la variable Service
+# ya que en el resultado anterior se observó que su coeficiente de regresión
+# no fue estadísticamente significativo (al considerar su p-value)
+
+# Y = beta0 + beta1*Food + beta2*Decor + beta4*East + e (Reducido)
+
+m2 <- lm(Price ~ Food + Decor + East)
+
+# Obtenemos un resumen del modelo ajustado
+
+summary(m2)
+
+# Una forma alternativa de obtener m2 es usar el comando update
+
+m2 <- update(m1, ~.-Service)
+summary(m2)
+
+#Gráficamos los residuales 
+plot(m2)
